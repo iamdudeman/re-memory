17cbeb2fb521e8a608b9c0d27dcc1c05eab8f8c4
iamdudeman <timothy.solum@gmail.com>
2024-01-02 21:41:25 -0600

    hooked up diary gui and deleted old files

--- game/src/main/java/technology/sola/engine/rememory/gui/GuiManager.java
+++ game/src/main/java/technology/sola/engine/rememory/gui/GuiManager.java
@@ -42,6 +42,15 @@ public class GuiManager {
       .setVisibility(Visibility.VISIBLE)
       .build()
   );
+  private final String[] pages = {
+    "  I don't know what's been going on recently. I've been feeling so confused about what I'm doing and forgetful of things I need to do or have been doing. Do I need more sleep? Do I need to change my diet? Is this just what getting older is like? Until I figure out what's happening, I'm going to start writing things down in this journal to make sure I remember the things I need to.",
+    "  This is worse than I could have possibly imagined. Just got off the phone with my doctor. He gave me the grim diagnosis. Alzheimer's. I should have known. The forgetfulness and confusion have only gotten worse. It feels like I get lost in my own home sometimes. Sometimes it takes a while to remember the names of my own children. I feel so scared and alone.",
+    "  Today I visited the old lighthouse my grandpa operated when I was a kid. It's the first time in a long time that I've felt any peace. Memories came flooding back. Memories of sitting on his lap as he'd tell me stories or sing old hymns. Like a light shining in darkness, this is the only place I can go to see clearly anymore.",
+    "  It's gotten so much worse. I can't think. I can't follow conversations. I can't remember my own children. I need people to guide me around my own home. The only time I can do anything is when I'm here at the lighthouse. It feels like my own brain wants to destroy me. I can't do anything right anymore because I've forgotten how. My loved ones are being taken from me by my own mind. I feel so hopeless. I'm so alone. God help me...",
+    "  I experience the memories of a stranger every day. Each day the same process of \"re-memory\" as I struggle to find and read through this diary. But even so, even if the \"me\" tomorrow is once again a stranger; even if the rest of my days are just re-memories of the past; I will trust in the Light that pierces even this growing darkness.                                                                                           John 1:5",
+    "~~~~~Thank you for playing~~~~~ Music-@Eliteomnomnivore Code-@iamdudeman      Art-@Denise      Art-@iamdudeman Art-@Eliteomnomnivore Story-@Lvl30Caterpie Story-@iamdudeman",
+  };
+  private int pageIndex = 0;
 
   private boolean showingPlayerMessage = false;
 
@@ -144,6 +153,36 @@ public class GuiManager {
           showingPlayerMessage = true;
         }
       );
+    });
+
+    // diary
+    var diaryTextElement = diaryDocument.rootElement().findElementById("diary", TextGuiElement.class);
+    var diaryContainerElement = diaryDocument.rootElement().findElementById("diary-container", SectionGuiElement.class);
+
+    diaryTextElement.setText(pages[pageIndex]);
+
+    final Runnable changePage = () -> {
+      if (pageIndex > pages.length - 1) {
+        pageIndex = pages.length - 1;
+      } else if (pageIndex < 0) {
+        pageIndex = 0;
+      }
+
+      diaryTextElement.setText(pages[pageIndex]);
+    };
+
+    diaryContainerElement.events().keyPressed().on(guiKeyEvent -> {
+      if (guiKeyEvent.getKeyEvent().keyCode() == Key.RIGHT.getCode() || guiKeyEvent.getKeyEvent().keyCode() == Key.SPACE.getCode()) {
+        pageIndex++;
+        changePage.run();
+      } else if (guiKeyEvent.getKeyEvent().keyCode() == Key.LEFT.getCode()) {
+        pageIndex--;
+        changePage.run();
+      }
+    });
+    diaryContainerElement.events().mousePressed().on(guiMouseEvent -> {
+      pageIndex++;
+      changePage.run();
     });
   }
 
